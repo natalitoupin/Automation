afa20f6fc7bd1190dc3cd8ea2356e3138ae7888d
Natali Toupin <natali.toupin@candis-MacBook-Air.local>
2015-01-03 16:24:54 -0500

    All the gems and google text

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 
-# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
@@ -13,6 +12,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -37,4 +37,15 @@ gem 'spring',        group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+
+group :development, :test do
+  gem 'byebug'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'selenium-webdriver'
+end
 
---
+++ spec/features/paperless_post_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+require 'uri'
+
+feature 'search_the_web', js: true do
+
+  scenario 'search_google' do
+    visit 'http://google.com'
+    fill_in "q", :with => "Paperless Post"
+    click_link 'Paperless Post: Custom cards and invitations'
+    expect(page).to have_css '.svg-logo-full'
+    expect(page.current_url).to eq "https:\/\/www.paperlesspost.com\/"
+    #expect_url_includes 'paperlesspost'
+
+  end
+
+  # scenario 'search_yahoo' do
+  #   visit 'http://yahoo.com'
+  #   fill_in "p_13838465-p", :with => "Paperless Post"
+  #   click_button "Search Web"
+  #   click_link 'Paperless Post - Official Site'
+  #   find('.svg-logo-full')
+  #   find ('#svg-logo-full').click
+  #   expect(page).to have_css '.svg-logo-full'
+  # end
+
+  # scenario 'search_bing' do
+  #   visit 'http://bing.com'
+  #   fill_in "q", :with => "Paperless Post"
+  #   find('.go').click
+  #   click_link 'Paperless Post - Official Site'
+  #   expect(page).to have_css '.svg-logo-full'
+  #   expect_url_includes 'paperlesspost'
+  # end
+end
+
+# Go to Google.
+# Search for "Paperless Post"
+# Assert that the first result is our home page by visiting it.
+# Do the same thing in Yahoo and Bing.
+# Keep track of how long it takes you. Feel free to look things up as you need to.
